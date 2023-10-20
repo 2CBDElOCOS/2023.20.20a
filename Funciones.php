9a6a869fc24ffcb69bf900a05c1af41256d3e204
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2023-10-20 12:13:07 -0500

    Update Funciones.php

--- Funciones.php
+++ Funciones.php
@@ -1,44 +1,16 @@
-<?php
-/**
- * Realiza una consulta a la base de datos y retorna los resultados de la tabla de usuarios como una cadena HTML.
- * @param  $U (Opcional) El ID del usuario para filtrar los resultados. Evita problemas de SQL injection al usar este parámetro.
- * @return Texto Devuelve una cadena HTML con los resultados de la consulta.
- */
-function consulta($U = null)
-{
-    $Salida = ""; // Se inicializa la variable como una cadena vacía
-    $conexion = mysqli_connect('localhost', 'root', 'root', 'practica_03'); // Se establece la conexión a la base de datos
-
-    $sql = "SELECT id_usuario, Nombre, Sitio, Invitacion FROM tb_personas"; // Consulta SQL
-
-    if ($U != null) {
-        $U = mysqli_real_escape_string($conexion, $U); // Evita problemas de SQL injection
-        $sql .= " WHERE id_usuario = '$U'";
-    }
-
-    $resultado = $conexion->query($sql); // Ejecuta la consulta
-
-    // Recorre el recordset
-    while ($fila = mysqli_fetch_assoc($resultado)) {
-        $Salida .= $fila['id_usuario'] . '<br>'; // Concatena el valor de la columna id_usuario a $Salida
-        $Salida .= $fila['Nombre'] . '<br>'; // Concatena el valor de la columna Nombre a $Salida
-        $Salida .= $fila['Sitio'] . '<br>'; // Concatena el valor de la columna Sitio a $Salida
-        $Salida .= $fila['Invitacion'] . '<br>'; // Concatena el valor de la columna Invitacion a $Salida
-        $Salida .= '<br>'; // Agrega un salto de línea adicional
-    }
-    $conexion->close(); // Se finaliza la conexión
-
-    return $Salida; // Retorna la salida con los resultados
-}
 /**
  * Realiza una consulta a la base de datos y retorna los resultados de la tabla de usuarios como una cadena HTML.
  *
- * @param  $u El ID de usuario a autenticar.
- * @param  $s La clave del usuario a autenticar.
- * @return Texto Devuelve una cadena HTML con los resultados de la consulta.
+ * @param string $u El ID de usuario a autenticar.
+ * @param string $s La clave del usuario a autenticar.
+ * @param int $c Parámetro que determina el tipo de consulta. Si es 1, se obtienen datos de usuarios; si es diferente de 1, se obtiene el conteo.
+ * @param int|null $limit Límite de resultados a obtener.
+ * @param string|null $columnas Columnas a seleccionar en la consulta. Si no se especifican, se seleccionan todas las columnas.
+ *
+ * @return string Devuelve una cadena HTML con los resultados de la consulta.
  */
-
- function autenticar($u = null, $s = null, $c = 1, $limit = null, $columnas = null) {
+function autenticar($u = null, $s = null, $c = 1, $limit = null, $columnas = null) {
+    // Conectar a la base de datos
     $conexion = mysqli_connect('localhost', 'root', 'root', 'practica_03');
 
     if (!$conexion) {
@@ -46,17 +18,21 @@ function consulta($U = null)
     }
 
     if ($c != 1) {
+        // Consulta para obtener el conteo de usuarios
         $sql = "SELECT COUNT(*) AS num_usuarios FROM tb_personas";
     } else {
         if ($u !== null && $s !== null) {
             $s = mysqli_real_escape_string($conexion, $s); // Evita SQL injection
             $columnas_select = $columnas ? $columnas : '*';
+            // Consulta para obtener datos de usuarios con ID y clave específicos
             $sql = "SELECT $columnas_select FROM tb_personas WHERE id_usuario = '$u' AND BINARY Clave = '$s'";
         } elseif ($u !== null) {
             $columnas_select = $columnas ? $columnas : 'id_usuario, Nombre, Sitio, Invitacion';
+            // Consulta para obtener datos de usuarios con ID específico
             $sql = "SELECT $columnas_select FROM tb_personas WHERE id_usuario = '$u'";
         } else {
             $columnas_select = $columnas ? $columnas : '*';
+            // Consulta para obtener todos los datos de usuarios
             $sql = "SELECT $columnas_select FROM tb_personas";
         }
 
@@ -66,30 +42,23 @@ function consulta($U = null)
         }
     }
 
+    // Ejecutar la consulta
     $resultado = $conexion->query($sql);
 
     if ($resultado) {
         if ($c != 1) {
+            // Si se solicitó el conteo, mostrar el número de usuarios
             $row = $resultado->fetch_assoc();
             return 'El número de usuarios existentes son: ' . $row['num_usuarios'];
         } else {
             $salida = '';
 
-            if ($columnas) {
-                $columnas_array = explode(',', $columnas);
-                while ($fila = mysqli_fetch_assoc($resultado)) {
-                    foreach ($columnas_array as $columna) {
-                        $salida .= "$columna: " . $fila[trim($columna)] . "<br>";
-                    }
-                    $salida .= '<br>';
+            while ($fila = mysqli_fetch_assoc($resultado)) {
+                // Mostrar los datos de los usuarios
+                foreach ($fila as $clave => $valor) {
+                    $salida .= "$clave: $valor <br>";
                 }
-            } else {
-                while ($fila = mysqli_fetch_assoc($resultado)) {
-                    foreach ($fila as $clave => $valor) {
-                        $salida .= "$clave: $valor <br>";
-                    }
-                    $salida .= '<br>';
-                }
+                $salida .= '<br>';
             }
 
             return $salida;

62d46e4f9b53a023189a5b25f8410eb2754ffff2
Salv_PorG <128723032+2CBDElOCOS@users.noreply.github.com>
2023-10-20 12:05:42 -0500

    Update Despliegue.php

--- Despliegue.php
+++ Despliegue.php
@@ -2,4 +2,11 @@
 //Se incluye el archivo funciones
 include('Funciones.php');
 
-echo consulta()."<br>"; //Se invoca la funcion con un espacio entre linea
+//echo consulta(1)."<br>"; //Se invoca la funcion con un espacio entre linea
+echo autenticar(null, null, 1, 10, 'id_usuario, Sitio');
+
+
+//$resultado = autenticar(null, null, 1, 10, 'id_usuario, Sitio');
+
+//$resultado = autenticar(null, null, 1, 2);
+//echo $resultado;
